ed91db87d75f08ebd6072c50a99c2b0298f651c7
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2017-09-17 20:20:10 -0400

    Big refactor for the RemoveReservedCommandEventsFromBuffer

--- Windows.Matic.v1.Client/Recorder/Handler/InputHandler.cs
+++ Windows.Matic.v1.Client/Recorder/Handler/InputHandler.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Windows.Matic.v1.Recorder.Session;
+using Windows.Matic.v1.Recorder.ReservedCommands;
 using Windows.Matic.v1.Task;
 using static Windows.Matic.v1.Recorder.Listener.InputListener;
 
@@ -16,6 +14,7 @@ namespace Windows.Matic.v1.Recorder.Handler
         private bool _firstEventHandled = false;
 
         private bool _reservedCommandInProgress = false;
+        private ReservedCommandChecker _reservedCommandChecker;
 
         private List<Keys> _activeKeys;
         private int _activeMouseButtons;
@@ -23,14 +22,14 @@ namespace Windows.Matic.v1.Recorder.Handler
 
         private InputRecorderMediator _mediatorInstance;
 
-        private Action ReservedCommandFoundAction;
-
         public InputHandler(InputRecorderMediator irm)
         {
             _mediatorInstance = irm;
             _activeMouseButtons = 0;
             _activeKeys = new List<Keys>();
+
             _inputEventsBuffer = new List<InputEvent>();
+            _reservedCommandChecker = new ReservedCommandChecker();
         }
 
         public int HandleKeyboardEventProc(int code, int wParam, ref KeyboardHookEventStruct lParam)
@@ -42,11 +41,11 @@ namespace Windows.Matic.v1.Recorder.Handler
                 Keys key = (Keys)lParam.vkCode;
                 if ((wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
                 {
-                    HandleNewKeyboardEvent(new KeyboardEvent(key, KeyEventType.Down, delay));
+                    HandleNewInputEvent(new KeyboardEvent(key, KeyEventType.Down, delay));
                 }
                 else if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP))
                 {
-                    HandleNewKeyboardEvent(new KeyboardEvent(key, KeyEventType.Up, delay));
+                    HandleNewInputEvent(new KeyboardEvent(key, KeyEventType.Up, delay));
                 }
             }
 
@@ -67,15 +66,15 @@ namespace Windows.Matic.v1.Recorder.Handler
 
                 if ((wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN))
                 {
-                    HandleNewMouseEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.Down, delay));
+                    HandleNewInputEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.Down, delay));
                 }
                 else if ((wParam == WM_LBUTTONUP || wParam == WM_RBUTTONUP))
                 {
-                    HandleNewMouseEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.Up, delay));
+                    HandleNewInputEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.Up, delay));
                 }
                 else
                 {
-                    HandleNewMouseEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.None, delay));
+                    HandleNewInputEvent(new MouseEvent(lParam.pt.x, lParam.pt.y, lParam.flags, lParam.mouseData, MouseEventType.None, delay));
                 }
             }
 
@@ -96,26 +95,27 @@ namespace Windows.Matic.v1.Recorder.Handler
             return delaySinceLastEvent;
         }
 
-        private void HandleNewKeyboardEvent(KeyboardEvent inputEvent)
+        private void HandleNewInputEvent(KeyboardEvent keyboardEvent)
         {
             if (!_reservedCommandInProgress)
             {
-                _inputEventsBuffer.Add(inputEvent);
+                _inputEventsBuffer.Add(keyboardEvent);
             }
 
-            if (inputEvent.EventType == KeyEventType.Down && !_reservedCommandInProgress && !_activeKeys.Contains(inputEvent.Key))
+            if (keyboardEvent.EventType == KeyEventType.Down && !_reservedCommandInProgress && !_activeKeys.Contains(keyboardEvent.Key))
             {
-                _activeKeys.Add(inputEvent.Key);
-                if (ReservedCommandFoundInActiveKeys())
+                _activeKeys.Add(keyboardEvent.Key);
+                CommandNames commandFound = _reservedCommandChecker.Check(_activeKeys);
+                if (commandFound != CommandNames.None)
                 {
                     _reservedCommandInProgress = true;
-                    ReservedCommandFoundAction?.Invoke();
+                    InvokeReservedCommandAction(commandFound);
                 }
             }
-            else if (inputEvent.EventType == KeyEventType.Up && _activeKeys.Contains(inputEvent.Key))
+            else if (keyboardEvent.EventType == KeyEventType.Up && _activeKeys.Contains(keyboardEvent.Key))
             {
-                _activeKeys.Remove(inputEvent.Key);
-                if (!_activeKeys.Any())
+                _activeKeys.Remove(keyboardEvent.Key);
+                if (!_activeKeys.Any() && _activeMouseButtons == 0)
                 {
                     _reservedCommandInProgress = false;
                     SendInputEventsBufferToMediator();
@@ -123,26 +123,26 @@ namespace Windows.Matic.v1.Recorder.Handler
             }
         }
 
-        private void HandleNewMouseEvent(MouseEvent inputEvent)
+        private void HandleNewInputEvent(MouseEvent mouseEvent)
         {
             if (!_reservedCommandInProgress)
             {
-                _inputEventsBuffer.Add(inputEvent);
+                _inputEventsBuffer.Add(mouseEvent);
             }
 
-            if (inputEvent.EventType == MouseEventType.Down && !_reservedCommandInProgress)
+            if (mouseEvent.EventType == MouseEventType.Down && !_reservedCommandInProgress)
             {
-                _activeMouseButtons += 1;
+                _activeMouseButtons++; ;
             }
-            else if (inputEvent.EventType == MouseEventType.Up)
+            else if (mouseEvent.EventType == MouseEventType.Up)
             {
-                _activeMouseButtons -= 1;
-                if (_activeMouseButtons == 0)
+                _activeMouseButtons--;
+                if (!_activeKeys.Any() && _activeMouseButtons == 0)
                 {
                     SendInputEventsBufferToMediator();
                 }
             }
-            else
+            else if (!_activeKeys.Any() && _activeMouseButtons == 0)
             {
                 SendInputEventsBufferToMediator();
             }
@@ -157,138 +157,44 @@ namespace Windows.Matic.v1.Recorder.Handler
             }
         }
 
-        private void HandleReservedCommandPause()
+        private void InvokeReservedCommandAction(CommandNames commandFound)
         {
-            ReservedCommandFoundAction = null;
-            RemoveReservedCommandEventsFromBuffer(_reservedKeyPause);
+            switch(commandFound)
+            {
+                case CommandNames.Pause:
+                    InvokeReservedCommandPause();
+                    break;
+                case CommandNames.Resume:
+                    InvokeReservedCommandResume();
+                    break;
+                case CommandNames.Finish:
+                    InvokeReservedCommandFinish();
+                    break;
+            }
+        }
+
+        private void InvokeReservedCommandPause()
+        {
+            _inputEventsBuffer = _reservedCommandChecker.RemoveReservedCommandEventsFromBuffer(_inputEventsBuffer, CommandNames.Pause);
             SendInputEventsBufferToMediator();
             _mediatorInstance.PauseRecording();
         }
 
-        private void HandleReservedCommandResume()
+        private void InvokeReservedCommandResume()
         {
-            ReservedCommandFoundAction = null;
-            RemoveReservedCommandEventsFromBuffer(_reservedKeyResume);
+            _inputEventsBuffer = _reservedCommandChecker.RemoveReservedCommandEventsFromBuffer(_inputEventsBuffer, CommandNames.Resume);
             SendInputEventsBufferToMediator();
             _mediatorInstance.ResumeRecording();
         }
 
-        private void HandleReservedCommandDone()
+        private void InvokeReservedCommandFinish()
         {
-            ReservedCommandFoundAction = null;
-            RemoveReservedCommandEventsFromBuffer(_reservedKeyDone);
+            _inputEventsBuffer = _reservedCommandChecker.RemoveReservedCommandEventsFromBuffer(_inputEventsBuffer, CommandNames.Finish);
             SendInputEventsBufferToMediator();
+            // Since we will stop recording, we should update the boolean here
+            _reservedCommandInProgress = false;
             _mediatorInstance.StopRecording();
         }
-
-        #region Reserved Commands
-
-        private Keys _reservedKeyPause = Keys.P;
-        private Keys _reservedKeyResume = Keys.R;
-        private Keys _reservedKeyDone = Keys.D;
-
-        
-
-        private void RemoveReservedCommandEventsFromBuffer(Keys reservedKey)
-        {
-            int iLastElement = _inputEventsBuffer.Count - 1;
-            KeyboardEvent lastKeyboardEvent = _inputEventsBuffer[iLastElement] as KeyboardEvent;
-
-            if (lastKeyboardEvent != null)
-            {
-                if (lastKeyboardEvent.Key == reservedKey)
-                {
-                    List<int> indexesToRemove = new List<int>();
-                    indexesToRemove.Add(iLastElement);
-
-                    KeyboardEvent beforeLastKeyboardEvent = _inputEventsBuffer[iLastElement - 1] as KeyboardEvent;
-
-                    if (beforeLastKeyboardEvent != null)
-                    {
-                        Keys beforeLastKey = beforeLastKeyboardEvent.Key;
-                        if (beforeLastKey == Keys.LMenu || beforeLastKey == Keys.RMenu)
-                        {
-                            indexesToRemove.Add(iLastElement - 1);
-
-                            bool isSuite = true;
-                            for (int i = iLastElement - 2; i >= 0 && isSuite; i--)
-                            {
-                                KeyboardEvent indexedKeyboardEvent = _inputEventsBuffer[i] as KeyboardEvent;
-
-                                if (indexedKeyboardEvent != null)
-                                {
-                                    if (indexedKeyboardEvent.Key == beforeLastKey)
-                                    {
-                                        indexesToRemove.Add(i);
-                                    }
-                                    else if (indexedKeyboardEvent.Key == Keys.LControlKey || indexedKeyboardEvent.Key == Keys.RControlKey)
-                                    {
-                                        indexesToRemove.Add(i);
-                                    }
-                                    else
-                                    {
-                                        isSuite = false;
-                                    }
-                                }
-                            }
-                        }
-                        else if (beforeLastKey == Keys.LControlKey || beforeLastKey == Keys.RControlKey)
-                        {
-                            indexesToRemove.Add(iLastElement - 1);
-
-                            bool isSuite = true;
-                            for (int i = iLastElement - 2; i >= 0 && isSuite; i--)
-                            {
-                                KeyboardEvent indexedKeyboardEvent = _inputEventsBuffer[i] as KeyboardEvent;
-
-                                if (indexedKeyboardEvent != null)
-                                {
-                                    if (indexedKeyboardEvent.Key == beforeLastKey)
-                                    {
-                                        indexesToRemove.Add(i);
-                                    }
-                                    else if (indexedKeyboardEvent.Key == Keys.LMenu || indexedKeyboardEvent.Key == Keys.RMenu)
-                                    {
-                                        indexesToRemove.Add(i);
-                                    }
-                                    else
-                                    {
-                                        isSuite = false;
-                                    }
-                                }
-                            }
-                        }
-
-                        foreach (int i in indexesToRemove)
-                        {
-                            _inputEventsBuffer.RemoveAt(i);
-                        }
-
-                        // Repass through the buffer and make sure no keys are being hold down
-                        // because of the use case where a user chains command with Ctrl and ends
-                        // up with a reserved command
-                        List<Keys> simulatedActiveKeys = new List<Keys>();
-                        foreach (KeyboardEvent ie in _inputEventsBuffer.OfType<KeyboardEvent>())
-                        {
-                            if (ie.EventType == KeyEventType.Down && !simulatedActiveKeys.Contains(ie.Key))
-                            {
-                                simulatedActiveKeys.Add(ie.Key);
-                            }
-                            else if (ie.EventType == KeyEventType.Up && simulatedActiveKeys.Contains(ie.Key))
-                            {
-                                simulatedActiveKeys.Remove(ie.Key);
-                            }
-                        }
-
-                        foreach (Keys k in simulatedActiveKeys)
-                        {
-                            _inputEventsBuffer.Add(new KeyboardEvent(k, KeyEventType.Down, 50));
-                        }
-                    }
-                }
-            }
-        }
-        #endregion
 
         /// <summary>
         /// Keyboard Message Identifiers
--- Windows.Matic.v1.Client/Recorder/ReservedCommands/ReservedCommandChecker.cs
+++ Windows.Matic.v1.Client/Recorder/ReservedCommands/ReservedCommandChecker.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
+using Windows.Matic.v1.Task;
 
 namespace Windows.Matic.v1.Recorder.ReservedCommands
 {
@@ -48,7 +50,6 @@ namespace Windows.Matic.v1.Recorder.ReservedCommands
                     }
                 }
             }
-
             return matchCount == keyModifiers.Count;
         }
 
@@ -57,7 +58,7 @@ namespace Windows.Matic.v1.Recorder.ReservedCommands
             switch(mk)
             {
                 case ModifierKeys.Alt:
-                    return k == Keys.LMenu || k == Keys.LMenu;
+                    return k == Keys.LMenu || k == Keys.RMenu;
                 case ModifierKeys.Ctrl:
                     return k == Keys.LControlKey || k == Keys.RControlKey;
                 case ModifierKeys.Shift:
@@ -67,6 +68,89 @@ namespace Windows.Matic.v1.Recorder.ReservedCommands
                 default:
                     return false;
             }
+        }
+
+        private bool KeyboardEventMatchesReservedCommandModifier(KeyboardEvent ke, ReservedCommand rc)
+        {
+            bool matchFound = false;
+            foreach(ModifierKeys mk in rc.KeyModifiers)
+            {
+                if (ActiveKeyIsKeyModifier(ke.Key, mk))
+                {
+                    matchFound = true;
+                    break;
+                }
+            }
+            return matchFound;
+        }
+
+        public List<InputEvent> RemoveReservedCommandEventsFromBuffer(List<InputEvent> buffer, CommandNames commandInvoked)
+        {
+            Keys finalKey = Keys.None;
+            if (commandInvoked != CommandNames.None)
+            {
+                finalKey = _reservedCommands[commandInvoked].FinalKey;
+            }
+
+            if (finalKey != Keys.None)
+            {
+                int iLastElement = buffer.Count - 1;
+                KeyboardEvent lastKeyboardEvent = buffer[iLastElement] as KeyboardEvent;
+
+                if (lastKeyboardEvent != null)
+                {
+                    if (lastKeyboardEvent.Key == finalKey)
+                    {
+                        List<int> indexesToRemove = new List<int>();
+                        indexesToRemove.Add(iLastElement);
+
+                        bool isSuite = true;
+                        for (int i = iLastElement - 1; i >= 0 && isSuite; i--)
+                        {
+                            KeyboardEvent indexedKeyboardEvent = buffer[i] as KeyboardEvent;
+                            if (indexedKeyboardEvent != null)
+                            {
+                                if (KeyboardEventMatchesReservedCommandModifier(indexedKeyboardEvent, _reservedCommands[commandInvoked]))
+                                {
+                                    indexesToRemove.Add(i);
+                                }
+                                else
+                                {
+                                    isSuite = false;
+                                }
+                            }
+                        }
+
+                        foreach (int i in indexesToRemove)
+                        {
+                            buffer.RemoveAt(i);
+                        }
+
+                        // Repass through the buffer and make sure no keys are being hold down
+                        // because of the use case where a user chains command with Ctrl and ends
+                        // up with a reserved command
+                        List<Keys> simulatedActiveKeys = new List<Keys>();
+                        foreach (KeyboardEvent ie in buffer.OfType<KeyboardEvent>())
+                        {
+                            if (ie.EventType == KeyEventType.Down && !simulatedActiveKeys.Contains(ie.Key))
+                            {
+                                simulatedActiveKeys.Add(ie.Key);
+                            }
+                            else if (ie.EventType == KeyEventType.Up && simulatedActiveKeys.Contains(ie.Key))
+                            {
+                                simulatedActiveKeys.Remove(ie.Key);
+                            }
+                        }
+
+                        foreach (Keys k in simulatedActiveKeys)
+                        {
+                            buffer.Add(new KeyboardEvent(k, KeyEventType.Down, 50));
+                        }
+                    }
+                }
+            }
+
+            return buffer;
         }
     }
 }
