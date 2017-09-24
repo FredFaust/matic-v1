2adf9a29e1305af76caef871f03e928a81e5e022
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2017-09-24 18:55:18 -0400

    Add profile... think about serialization

---
+++ Windows.Matic.v1.Client/Profile/UserProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Matic.v1.Core.Task;
+
+namespace Windows.Matic.v1.Client.Profile
+{
+    public class UserProfile
+    {
+        private string _name;
+        private List<ComputerTask> _computerTasks;
+
+        public UserProfile(string name)
+        {
+            _name = name;
+            _computerTasks = new List<ComputerTask>();
+        }
+
+        public void AddComputerTask(ComputerTask task)
+        {
+            _computerTasks.Add(task);
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
